5a8445c52e8299f96327e9833752cd8b47b40f14
Ivan Rocha <104407334+Ivanrocha10@users.noreply.github.com>
2022-10-27 18:45:53 -0300

    Erro de digitação corrigido

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var isIgnite = false
 
 function changeCard(event) {
   var card = event.currentTarget
-  var backgroundImage = isIgnite ? "explorer" : "ignite"
+  var bg = isIgnite ? "explorer" : "ignite"
   isIgnite = !isIgnite
-  card.style.backgroundImage = 'url(./svg/bg-${bg}.svg)'
+  card.style.backgroundImage = `url(./svg/bg-${bg}.svg)`
 }
